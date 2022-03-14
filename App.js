355e964bf7b38fd2867335da46c320e7bdc62974
Kazuki Ichinose <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2022-03-14 11:51:27 +0900

    init

--- App.js
+++ App.js
@@ -1,16 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
 import StartPage from './pages/StartPage';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <StartPage />;
 }
 
 const styles = StyleSheet.create({
