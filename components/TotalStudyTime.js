9d4e7caa53b47c277a0cceadf2294c09b3b0f987
Kazuki Ichinose <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2022-03-15 15:51:04 +0900

    add save timer on database

--- components/TotalStudyTime.js
+++ components/TotalStudyTime.js
@@ -1,11 +1,60 @@
-import React from 'react';
+// 一旦セーブしないとsecondsが更新されない
+
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Image, Text } from 'react-native';
 
 const TotalStudyTime = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [updatingSeconds, setUpdatingSeconds] = useState(true);
+
+  useEffect(() => {
+    fetch(
+      'https://nskserver-97f50-default-rtdb.firebaseio.com/timer/timer.json'
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        if (data !== null) {
+          setSeconds(data.seconds);
+        }
+      });
+    setUpdatingSeconds(false);
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (updatingSeconds !== true) {
+        setSeconds((seconds) => seconds + 1);
+      }
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  useEffect(() => {
+    fetch(
+      'https://nskserver-97f50-default-rtdb.firebaseio.com/timer/timer.json',
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          seconds,
+        }),
+      }
+    );
+  }, [seconds]);
+
   return (
     <View style={styles.container}>
       <Image style={styles.image} source={require('../assets/favicon.png')} />
-      <Text style={styles.text}>9時間41分</Text>
+      <Text style={styles.text}>
+        {Math.trunc(seconds / 3600)}時間{Math.trunc((seconds % 3600) / 60)}分
+        {Math.trunc(seconds % 60)}
+      </Text>
     </View>
   );
 };
