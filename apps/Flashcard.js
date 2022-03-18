6cc82dc08fdd33115a2b8c1734ac45aa9408b669
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2022-03-18 11:26:40 +0900

    nothing to commit, can be deleted

--- apps/Flashcard.js
+++ apps/Flashcard.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import vocab from '../assets/flashcard.json';
+
 const Flashcard = () => {
-  return;
+  const random_index = Math.floor(Math.random() * vocab.length);
+  let random_false_index = [];
+  while (random_false_index.length < 3) {
+    const r = Math.floor(Math.random() * vocab.length);
+    for (let i = 0; i < random_false_index.length; ++i) {
+      if (r == random_false_index[i]) {
+        continue;
+      }
+    }
+    if (r == random_index) {
+      continue;
+    }
+
+    random_false_index.push(r);
+  }
+  const word = vocab[random_index];
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.word}>{word.English}</Text>
+      <Text style={styles.description}>{word.Japanese}</Text>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+  word: {},
+  description: {},
+});
+
+export default Flashcard;
