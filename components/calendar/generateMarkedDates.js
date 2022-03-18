536490adfc9b197541e6e00fff44bbc1c7412cee
NT-marlowe <n.tezuka196.ninj@gmail.com>
2022-03-18 14:41:09 +0900

    feat: modify loginDates colors

--- components/calendar/generateMarkedDates.js
+++ components/calendar/generateMarkedDates.js
@@ -2,7 +2,8 @@ const generateMarkedDates = (loginDates) => {
   let res = {};
   loginDates.forEach((date) => {
     const dateString = '2022-03-' + date;
-    const setValue = { selected: true, marked: true, selectedColor: 'blue' };
+    const color = date == new Date().getDate() ? '#6495ED' : '#7FFFD4';
+    const setValue = { selected: true, marked: true, selectedColor: color };
     res[dateString] = setValue;
   });
 
